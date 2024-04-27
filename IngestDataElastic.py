1c0fc278c69a95d0a004e57ac6ce19ad377370f5
Athuri <30321620+Athuriviveka@users.noreply.github.com>
2024-04-26 21:13:07 -0400

    Update IngestDataElastic.py

--- IngestDataElastic.py
+++ IngestDataElastic.py
@@ -1,7 +1,7 @@
 from elasticsearch import Elasticsearch
 
 def ingest(data, i):
-    es = Elasticsearch(["https://localhost:9200"], basic_auth=('elastic', 'Lp9PHgG_ng3grN_ixQMi'))
+    es = Elasticsearch(["https://localhost:9200"], basic_auth=('jghggdf', 'passwor'))
     es.info()
     es.index(index='user_data', id=i, document=data)
 
